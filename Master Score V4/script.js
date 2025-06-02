9f4cf5aa44aecb728d157514b638c27f02adf9fd
Wazzaabi <123635431+Wazzaabi@users.noreply.github.com>
2025-06-03 01:51:15 +0200

    Delete Master Score V4 directory

--- Master Score V4/script.js
+++
@@ -1,9 +0,0 @@
-// Désactive “Reprendre” si pas de partie en cours
-document.addEventListener('DOMContentLoaded', () => {
-  const cont = document.getElementById('reprendre-container');
-  if (!localStorage.getItem('currentGame')) {
-    cont.classList.add('disabled');
-  } else {
-    cont.classList.remove('disabled');
-  }
-});
